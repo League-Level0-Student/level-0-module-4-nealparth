29e3228e8a33af8ed7e184e4dbd9a05c0eae267c
frs123456789 <league@WTS7.local>
2018-04-30 18:04:46 -0700

    OwnAdventure

---
+++ src/OwnAdventure.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+	int plot=  JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Lakers", "author", "architect" }, null);
+	if(plot==0) {
+		int next= JOptionPane.showOptionDialog(null, "What do you want to be?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "GM", "Manager", "Player" }, null);
+		int next1= JOptionPane.showOptionDialog(null, "What's your descision now", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Retire", "DO it", "Think about it" }, null);
+		
+		
+	}
+	
+	else if(plot==1) {
+		int further= JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "fantasy", "mystery", "action" }, null);
+		int further1= JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Magic", "Mythical Creatures", "Gods" }, null);
+	}
+	else if(plot==2) {
+		int after= JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "building", "bridge", "park" }, null);
+		int after1= JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Keep Building", "Quit", "Take a break" }, null);
+		
+	}
+}
+}
